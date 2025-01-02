0149a685b06905e56ff916c19f774815de98df9f
QUES <97351587+Ques-yt@users.noreply.github.com>
2025-01-03 01:58:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,16 +11,16 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'QUES',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
 	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	greetingMorning: 'Good Morning!',
+	greetingAfternoon: 'Good Afternoon!,',
+	greetingEvening: 'Good Evening!,',
+	greetingNight: 'Good Night!',
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
